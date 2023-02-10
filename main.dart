89b39405ea5a730f0136c559be0cf3e8d035fa43
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-10 16:52:37 +0300

    Stack widget created
    Stack widget with a container inside. On top of the container a text is inserted.

--- main.dart
+++ main.dart
@@ -11,13 +11,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack widget layout'),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('HEY'),
-              Text('I want to use stack widget to layout widgets one on top of the other'),
-              Text('This is the start'),
+            children: <Widget>[
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.blueGrey,
+                    height: 100,
+                  ),
+                  const Text(
+                    'Here is a text on top of a container in stack widget',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
+              const Text('Hurray!',style: TextStyle( fontSize: 20),),
             ],
           ),
         ),
